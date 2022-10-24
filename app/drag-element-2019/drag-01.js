843405e08816df39ce79097be2d744e76fa75a3a
alphabetX <alphabet_working_pc@inner.mail>
2022-10-24 19:11:42 +0800

    feat(drag-element-2019): 样式调整

--- app/drag-element-2019/drag-01.js
+++ app/drag-element-2019/drag-01.js
@@ -1,17 +1,15 @@
-import {
-  select,
-} from './../../common/util-dom.js';
+import { select } from "./../../common/util-dom.js";
 
-const dragEl =  select('#dragEl');
+const dragEl = select("#dragEl");
 
-  const width = dragEl.offsetWidth;
-  const height = dragEl.offsetHeight;
+const width = dragEl.offsetWidth;
+const height = dragEl.offsetHeight;
 
-  // 边界
-  const maxLeft = 1000 - width;
-  const maxTop = 800 - height;
+// 边界
+const maxLeft = 1000 - width;
+const maxTop = 800 - height;
 
-  dragEl.onmousedown = function (ev) {
+dragEl.onmousedown = function (ev) {
     ev.stopPropagation();
     const thisEl = this;
 
@@ -25,36 +23,36 @@ const dragEl =  select('#dragEl');
 
     // 计算位置
     const moveFn = function (ev) {
-      // 移动的位置
-      const moveX = ev.clientX;
-      const moveY = ev.clientY;
+        // 移动的位置
+        const moveX = ev.clientX;
+        const moveY = ev.clientY;
 
-      // 位置 = (移动位置 - 点击的位置) + 元素的初始位置
-      // 移动位置 - 点击的位置 ===> 这个计算的鼠标移动的位置差--变化量
-      let posX = moveX - currentX + offsetX;
-      let posY = moveY - currentY + offsetY;
+        // 位置 = (移动位置 - 点击的位置) + 元素的初始位置
+        // 移动位置 - 点击的位置 ===> 这个计算的鼠标移动的位置差--变化量
+        let posX = moveX - currentX + offsetX;
+        let posY = moveY - currentY + offsetY;
 
-      // 边界位置判断
-      if (posX >= maxLeft) posX = maxLeft;
-      if (posX <= 0) posX = 0;
-      if (posY >= maxTop) posY = maxTop;
-      if (posY <= 0) posY = 0;
+        // 边界位置判断
+        if (posX >= maxLeft) posX = maxLeft;
+        if (posX <= 0) posX = 0;
+        if (posY >= maxTop) posY = maxTop;
+        if (posY <= 0) posY = 0;
 
-      // 对元素的位置进行赋值
-      thisEl.style.left = posX + 'px';
-      thisEl.style.top = posY + 'px';
+        // 对元素的位置进行赋值
+        thisEl.style.left = posX + "px";
+        thisEl.style.top = posY + "px";
     };
 
     // document-mouseup
     // 移出边界后取消事件监听--->在任何时候，鼠标抬起之后就应该取消事件
     const docMouseupHandle = function () {
-      document.removeEventListener('mousemove', moveFn);
-      document.removeEventListener('mouseup', docMouseupHandle);
+        document.removeEventListener("mousemove", moveFn);
+        document.removeEventListener("mouseup", docMouseupHandle);
     };
 
     // move
-    document.addEventListener('mousemove', moveFn, false);
+    document.addEventListener("mousemove", moveFn, false);
 
     // 停止监听move
-    document.addEventListener('mouseup', docMouseupHandle, false);
-  };
+    document.addEventListener("mouseup", docMouseupHandle, false);
+};
